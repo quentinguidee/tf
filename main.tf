6ba2354a4efb33f0cf8cdda9c7ee4201c5b0b1b8
Quentin Guidée <git@arra.red>
2024-01-13 08:20:55 -0500

    Configure the ingress

--- main.tf
+++ main.tf
@@ -1,6 +1,149 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.25.2"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
+  }
+}
+
+resource "kubernetes_ingress_v1" "vertex_ingress" {
+  metadata {
+    name = "vertex-ingress"
+    annotations = {
+      "kubernetes.io/ingress.class" = "traefik"
+    }
+  }
+
+  spec {
+    rule {
+      host = var.vertex_urls.admin
+      http {
+        path {
+          backend {
+            service {
+              name = "vertex-admin-vertex-admin-service"
+              port {
+                number = 7000
+              }
+            }
+          }
+          path = "/"
+        }
+      }
+    }
+
+    rule {
+      host = var.vertex_urls.auth
+      http {
+        path {
+          backend {
+            service {
+              name = "vertex-auth-vertex-auth-service"
+              port {
+                number = 7002
+              }
+            }
+          }
+          path = "/"
+        }
+      }
+    }
+
+    rule {
+      host = var.vertex_urls.containers
+      http {
+        path {
+          backend {
+            service {
+              name = "vertex-containers-vertex-containers-service"
+              port {
+                number = 7004
+              }
+            }
+          }
+          path = "/"
+        }
+      }
+    }
+
+    rule {
+      host = var.vertex_urls.monitoring
+      http {
+        path {
+          backend {
+            service {
+              name = "vertex-monitoring-vertex-monitoring-service"
+              port {
+                number = 7006
+              }
+            }
+          }
+          path = "/"
+        }
+      }
+    }
+
+    rule {
+      host = var.vertex_urls.reverseproxy
+      http {
+        path {
+          backend {
+            service {
+              name = "vertex-reverseproxy-vertex-reverseproxy-service"
+              port {
+                number = 7008
+              }
+            }
+          }
+          path = "/"
+        }
+      }
+    }
+
+    rule {
+      host = var.vertex_urls.sql
+      http {
+        path {
+          backend {
+            service {
+              name = "vertex-sql-vertex-sql-service"
+              port {
+                number = 7012
+              }
+            }
+          }
+          path = "/"
+        }
+      }
+    }
+
+    rule {
+      host = var.vertex_urls.tunnels
+      http {
+        path {
+          backend {
+            service {
+              name = "vertex-tunnels-vertex-tunnels-service"
+              port {
+                number = 7014
+              }
+            }
+          }
+          path = "/"
+        }
+      }
+    }
   }
 }
 
